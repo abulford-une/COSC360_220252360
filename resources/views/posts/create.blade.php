453fbd657337d283db60b00588c0e0b5dfe7c434
abulford <a@b.cus>
2024-08-03 13:37:15 +1000

    lecture week 5 - modified create and show blades to match current lecture material

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
     <h1>Blog Post Create</h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="{{ route('posts.store') }}" method="POST">
         @csrf
         <div class="form-group">
