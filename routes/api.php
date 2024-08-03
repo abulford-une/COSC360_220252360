28c2955cf9216b8d957254e16086328dd323767c
abulford <a@b.cus>
2024-08-03 13:31:28 +1000

    lecture week 5 - created AuthController and configured route api.php for AuthController.login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', [UserController::class, 'index']);//->middleware('auth:sanctum');
+Route::get('/user', [UserController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
