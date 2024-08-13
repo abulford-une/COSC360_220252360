8bc9068ef37851ac11d13778406f02aa344bd27f
abulford <a@b.cus>
2024-08-13 16:44:32 +1000

    A2 step 4_3 - modified adminmiddleware to redirect to the welcome page with an error where the current user's user_role is not an admin, modified web.php to utilise adminmiddleware for all admin post routes

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -15,6 +16,10 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
+        $role = Auth::user()->user_role;
+        if ($role != 'admin') {
+            return redirect('')->with('error', 'You must be an admin to access this resource');
+        }
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,26 +20,29 @@ Route::group(['middleware' => ['auth']], function() {
         'update' => 'posts.update',
         'destroy' => 'posts.destroy',
     ]);
-    Route::resource('admin/posts', AdminPostController::class)->names([
-        'index' => 'admin.posts.index',
-        'create' => 'admin.posts.create',
-        'store' => 'admin.posts.store',
-        'show' => 'admin.posts.show',
-        'edit' => 'admin.posts.edit',
-        'update' => 'admin.posts.update',
-        'destroy' => 'admin.posts.destroy',
-    ]);
     Route::resource('author/posts', AuthorPostController::class)->names([
-        'index' => 'author.posts.index',
-        'create' => 'author.posts.create',
-        'store' => 'author.posts.store',
-        'show' => 'author.posts.show',
-        'edit' => 'author.posts.edit',
-        'update' => 'author.posts.update',
-        'destroy' => 'author.posts.destroy',
+        'index' => 'author.index',
+        'create' => 'author.create',
+        'store' => 'author.store',
+        'show' => 'author.show',
+        'edit' => 'author.edit',
+        'update' => 'author.update',
+        'destroy' => 'author.destroy',
     ]);
     Route::get('/something', [PostController::class, 'something']);
     Route::get('/home', [PostController::class, 'index'])->name('home');
+});
+
+Route::group(['middleware' => ['auth', 'admid']], function() {
+    Route::resource('admin/posts', AdminPostController::class)->names([
+        'index' => 'admin.index',
+        'create' => 'admin.create',
+        'store' => 'admin.store',
+        'show' => 'admin.show',
+        'edit' => 'admin.edit',
+        'update' => 'admin.update',
+        'destroy' => 'admin.destroy',
+    ]);
 });
 
 Auth::routes();
