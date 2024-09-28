9b4b69a2e108dca878a9b4bb5a9e9abab93e8671
abulford <a@b.cus>
2024-09-28 12:05:23 +1000

    A4 Backend - Reverted database connection to mysql

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use MongoDB\Laravel\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('MONGO_DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
